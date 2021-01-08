4309f56cf0117b72597cecc44892b3da7a4b04a6
Andrey <shock.work@mail.ru>
2021-01-09 01:11:08 +0300

    map component [template]

--- src/components/home/gcMap.vue
+++ src/components/home/gcMap.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="map"></div>
+  <div class="map">
+    <div class="map__info">
+      <p class="map__info-address__subtitle small">Адрес главного офиса и офлайн-магазина:</p>
+      <p class="map__info-address__title big">ул. Большая Конюшенная 19/8, Санкт-Петербург</p>
+      <p class="map__info-phone__subtitle small">Для заказов по телефону:</p>
+      <p class="map__info-phone__title big">8 812 450-25-25</p>
+      <p class="map__info-phone__more small">(с 10 до 20 ежедневно)</p>
+      <button class="map__info-btn def-btn">Форма обратной связи</button>
+    </div>
+  </div>
 </template>
 
 <script>
