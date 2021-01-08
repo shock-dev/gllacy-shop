42cd2363120fc8ad8bd6f96718a89fb25a9a5245
Andrey <shock.work@mail.ru>
2021-01-09 00:24:11 +0300

    sales component [template]

---
+++ src/components/home/gcSales.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="sales flex">
+    <div class="sales__item raspberry">
+      <div class="sales__item-title">Малинка даром!</div>
+      <p class="sales__item-text">При покупке 2 кг любого фруктового мороженого, добавим в ваш заказ банку малинового варенья бесплатно.</p>
+      <button class="sales__item-btn def-btn">Хочу варенье!</button>
+    </div>
+    <div class="sales__item chocolate">
+      <div class="sales__item-title">Шоколадки даром!</div>
+      <p class="sales__item-text">При покупке 2 кг пломбира, добавим в ваш заказ упаковку вкуснейшей шоколадной присыпки совершенно бесплатно.</p>
+      <button class="sales__item-btn def-btn">Хочу шоколадки!</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "gcSales"
+}
+</script>
