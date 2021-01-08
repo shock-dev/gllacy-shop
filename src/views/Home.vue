47837e8404367d716be7b73d05c6e72a9dadbc96
Andrey <shock.work@mail.ru>
2021-01-09 00:31:16 +0300

    includes all component for Home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,28 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <gc-slider/>
+    <gc-sales/>
+    <gc-hits/>
+    <gc-about/>
+    <gc-more/>
+  </div>
 </template>
 
 <script>
+import GcSlider from "@/components/home/gcSlider";
+import GcSales from "@/components/home/gcSales";
+import GcHits from "@/components/home/gcHits";
+import GcAbout from "@/components/home/gcAbout";
+import GcMore from "@/components/home/gcMore";
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    GcMore,
+    GcAbout,
+    GcHits,
+    GcSales,
+    GcSlider
+  }
 }
 </script>
