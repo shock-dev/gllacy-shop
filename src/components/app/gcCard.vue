613eb52d6a29c847c8992af201c7cb6ce8dc716d
Andrey <shock.work@mail.ru>
2021-01-09 00:27:25 +0300

    card component [template]

---
+++ src/components/app/gcCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="card__wrapper">
+    <router-link class="card" :class="{ hit }" to="#">
+      <div class="card__image">
+        <img class="card__hit" src="~@/assets/img/hit.svg" alt="hit">
+        <img src="~@/assets/img/ice-cream-1.jpg" alt="Сливочно-клубничное с присыпкой из белого шоколада">
+        <div class="card__price flex">
+          <div class="card__price-num">355</div>
+          <div class="card__price-rub">₽<span class="card__price-kg">/кг</span></div>
+        </div>
+      </div>
+      <div class="card__title">Сливочно-клубничное с присыпкой из белого шоколада</div>
+    </router-link>
+    <button class="card__btn def-btn">Быстрый просмотр</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "gcCard",
+  props: {
+    hit: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
