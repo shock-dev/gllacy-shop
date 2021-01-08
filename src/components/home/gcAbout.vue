19a420e2a06301f5dd1d256a4f0cdee4a158b3a3
Andrey <shock.work@mail.ru>
2021-01-09 00:29:50 +0300

    about component [template]

---
+++ src/components/home/gcAbout.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="about">
+    <div class="about__title">Магазин Глэйси — это онлайн- и офлайн-магазин по продаже мороженого собственного производства на развес</div>
+    <div class="about__items flex">
+      <div class="about__item flex">
+        <img src="~@/assets/img/ice-cream.svg" alt="">
+        <p class="about__text">
+          Все наше мороженое изготавливается на собственном производстве, с использованием современного оборудования и проверенных временем технологий.
+        </p>
+      </div>
+      <div class="about__item flex">
+        <img src="~@/assets/img/cow.svg" alt="">
+        <p class="about__text">
+          Закупка ингредиентов  производится только у проверенных фермерских хозяйств и компаний, с которыми нас связывает долговременное сотрудничество.
+        </p>
+      </div>
+      <div class="about__item flex">
+        <img src="~@/assets/img/eco.svg" alt="">
+        <p class="about__text">
+          Для приготовления мороженого используются настоящие сливки и молоко высочайшего качества. Все дополнительные ингредиенты и добавки произведены из натурального, экологически чистого сырья.
+        </p>
+      </div>
+      <div class="about__item flex">
+        <img src="~@/assets/img/thermometer.svg" alt="">
+        <p class="about__text">
+          Доставка нашего мороженого до заказчиков осуществляется в специальном термопаке, который не дает мороженому растаять в пути и позволяет сохранить превосходный вкус.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "gcAbout"
+}
+</script>
