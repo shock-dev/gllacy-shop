70b99b2e856acc4e7d4db5ecd45ae6bda4348d65
Andrey <shock.work@mail.ru>
2021-01-09 00:21:44 +0300

    slider component

---
+++ src/components/home/gcSlider.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="slider">
+    <hooper :center-mode="true">
+      <slide>
+        <img src="~@/assets/img/slider-img-1.png" alt="">
+        <h2 class="slider__title">Крем-брюле и пломбир с малиновым джемом</h2>
+        <button class="slider__btn def-btn">Давайте оба!</button>
+      </slide>
+      <slide>
+        <img src="~@/assets/img/slider-img-1.png" alt="">
+        <h2 class="slider__title">Крем-брюле и пломбир с малиновым джемом</h2>
+        <button class="slider__btn def-btn">Давайте оба!</button>
+      </slide>
+      <slide>
+        <img src="~@/assets/img/slider-img-1.png" alt="">
+        <h2 class="slider__title">Крем-брюле и пломбир с малиновым джемом</h2>
+        <button class="slider__btn def-btn">Давайте оба!</button>
+      </slide>
+      <hooper-pagination slot="hooper-addons"></hooper-pagination>
+    </hooper>
+  </div>
+</template>
+
+<script>
+import 'hooper/dist/hooper.css';
+
+import {
+  Hooper,
+  Slide,
+  Pagination as HooperPagination
+} from 'hooper';
+
+export default {
+  name: "gcSlider",
+  components: {
+    Hooper,
+    Slide,
+    HooperPagination
+  }
+}
+</script>
