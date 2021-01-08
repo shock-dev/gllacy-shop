7101f15779ca0ea5cddb9aab9fffc748412bd12d
Andrey <shock.work@mail.ru>
2021-01-09 00:28:27 +0300

    hits component for outputting hit products

---
+++ src/components/home/gcHits.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hits flex">
+    <div class="hits__item">
+      <gc-card :hit="true"/>
+    </div>
+    <div class="hits__item">
+      <gc-card :hit="true"/>
+    </div>
+    <div class="hits__item">
+      <gc-card :hit="true"/>
+    </div>
+    <div class="hits__item">
+      <gc-card :hit="true"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import GcCard from "@/components/app/gcCard";
+
+export default {
+  name: "gcHits",
+  components: {
+    GcCard
+  }
+}
+</script>
