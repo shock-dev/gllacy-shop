b97071612ed808a439e45ceeae5aa9d788929bcf
Andrey <shock.work@mail.ru>
2021-01-09 16:26:43 +0300

    new component ~ Sort

---
+++ src/components/catalog/gcSort.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="sort">
+    <div class="sort__top flex"></div>
+    <div class="sort__bottom flex"></div>
+  </div>
+</template>
+
+<script>
+export default {
+name: "gcSort"
+}
+</script>
--- src/views/Catalog.vue
+++ src/views/Catalog.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="catalog">
     <gc-breadcrumbs/>
+    <h2 class="catalog__title">Сливочное мороженое</h2>
+    <gc-sort/>
   </div>
 </template>
 
 <script>
 import GcBreadcrumbs from "@/components/catalog/gcBreadcrumbs";
+import GcSort from "@/components/catalog/gcSort";
 
 export default {
   name: "Catalog",
   components: {
+    GcSort,
     GcBreadcrumbs
   }
 }
