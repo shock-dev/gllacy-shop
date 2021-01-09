14b20da6da03e61d24f55dc38f39a01dbfed6933
Andrey <shock.work@mail.ru>
2021-01-09 03:35:34 +0300

    output data in component

--- src/components/app/gcCard.vue
+++ src/components/app/gcCard.vue
@@ -3,13 +3,13 @@
     <router-link class="card" :class="{ hit }" to="#">
       <div class="card__image">
         <img class="card__hit" src="~@/assets/img/hit.svg" alt="hit">
-        <img src="~@/assets/img/ice-cream-1.jpg" alt="Сливочно-клубничное с присыпкой из белого шоколада">
+        <img :src="imageUrl" :alt="title">
         <div class="card__price flex">
-          <div class="card__price-num">355</div>
+          <div class="card__price-num">{{ price }}</div>
           <div class="card__price-rub">₽<span class="card__price-kg">/кг</span></div>
         </div>
       </div>
-      <div class="card__title">Сливочно-клубничное с присыпкой из белого шоколада</div>
+      <div class="card__title">{{ title }}</div>
     </router-link>
     <button class="card__btn def-btn">Быстрый просмотр</button>
   </div>
@@ -19,6 +19,9 @@
 export default {
   name: "gcCard",
   props: {
+    title: String,
+    price: Number,
+    imageUrl: String,
     hit: {
       type: Boolean,
       default: false
--- src/components/home/gcHits.vue
+++ src/components/home/gcHits.vue
@@ -1,27 +1,29 @@
 <template>
   <div class="hits flex">
-    <div class="hits__item">
-      <gc-card :hit="true"/>
-    </div>
-    <div class="hits__item">
-      <gc-card :hit="true"/>
-    </div>
-    <div class="hits__item">
-      <gc-card :hit="true"/>
-    </div>
-    <div class="hits__item">
-      <gc-card :hit="true"/>
+    <div class="hits__item" v-for="({ title, price, imageUrl }, index) in hitProducts" :key="index">
+      <gc-card
+        :title="title"
+        :price="price"
+        :imageUrl="imageUrl"
+        :hit="true"
+      />
     </div>
   </div>
 </template>
 
 <script>
 import GcCard from "@/components/app/gcCard";
+import { mapGetters } from 'vuex'
 
 export default {
   name: "gcHits",
   components: {
     GcCard
+  },
+  computed: {
+    ...mapGetters('hits', {
+      hitProducts: 'items'
+    })
   }
 }
 </script>
