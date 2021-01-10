587866b2c47638c024f22884178b53ba9ac92676
Andrey <shock.work@mail.ru>
2021-01-10 05:54:47 +0300

    integrate products component in Catalog page

--- src/views/Catalog.vue
+++ src/views/Catalog.vue
@@ -3,16 +3,19 @@
     <gc-breadcrumbs/>
     <h2 class="catalog__title">Сливочное мороженое</h2>
     <gc-sort/>
+    <gc-products/>
   </div>
 </template>
 
 <script>
 import GcBreadcrumbs from "@/components/catalog/gcBreadcrumbs";
 import GcSort from "@/components/catalog/gcSort";
+import GcProducts from "@/components/catalog/gcProducts";
 
 export default {
   name: "Catalog",
   components: {
+    GcProducts,
     GcSort,
     GcBreadcrumbs
   }
