0c0f69a11866b3e7284121a0c7c3a112d01eb2e0
Andrey <shock.work@mail.ru>
2021-01-10 08:03:11 +0300

    radio list template

--- src/components/catalog/gcSort.vue
+++ src/components/catalog/gcSort.vue
@@ -5,6 +5,41 @@
         <div class="sort__col-title">Сортировка:</div>
         <div class="sort__col-wrapper"></div>
       </div>
+      <div class="sort__col">
+        <div class="sort__col-title">Наполнители:</div>
+        <div class="sort__col-box toggle">
+          <ul class="toggle__list flex">
+            <li class="toggle__item">
+              <label class="toggle__label">
+                <input class="toggle__real" type="radio" name="radio">
+                <span class="toggle__fake"></span>
+                <span class="toggle__text">0%</span>
+              </label>
+            </li>
+            <li class="toggle__item">
+              <label class="toggle__label">
+                <input class="toggle__real" type="radio" name="radio">
+                <span class="toggle__fake"></span>
+                <span class="toggle__text">до 10%</span>
+              </label>
+            </li>
+            <li class="toggle__item">
+              <label class="toggle__label">
+                <input class="toggle__real" type="radio" name="radio">
+                <span class="toggle__fake"></span>
+                <span class="toggle__text">до 30%</span>
+              </label>
+            </li>
+            <li class="toggle__item">
+              <label class="toggle__label">
+                <input class="toggle__real" type="radio" name="radio">
+                <span class="toggle__fake"></span>
+                <span class="toggle__text">выше 30%</span>
+              </label>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
     <div class="sort__row">
       <div class="sort__col">
