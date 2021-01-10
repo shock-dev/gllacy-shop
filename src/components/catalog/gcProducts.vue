7103fc93a347f332a97a3f02b18b709732041f70
Andrey <shock.work@mail.ru>
2021-01-10 05:51:22 +0300

    component for outputting data from module products

---
+++ src/components/catalog/gcProducts.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="products flex">
+    <div class="products__item" v-for="({ title, price, imageUrl }, index) in products" :key="index">
+      <gc-card
+        :title="title"
+        :price="price"
+        :imageUrl="imageUrl"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import GcCard from "@/components/app/gcCard";
+import { mapGetters } from 'vuex'
+
+export default {
+  name: "gcProducts",
+  components: {
+    GcCard
+  },
+  computed: {
+    ...mapGetters('products', {
+      products: 'items'
+    })
+  }
+}
+</script>
