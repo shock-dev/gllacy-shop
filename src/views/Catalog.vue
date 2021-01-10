3f8eb974bdd86de52715c50f45ceceeb784e4327
Andrey <shock.work@mail.ru>
2021-01-10 11:03:29 +0300

    integrate pagination component to Catalog page

--- src/views/Catalog.vue
+++ src/views/Catalog.vue
@@ -4,6 +4,7 @@
     <h2 class="catalog__title">Сливочное мороженое</h2>
     <gc-sort/>
     <gc-products/>
+    <gc-pagination/>
   </div>
 </template>
 
@@ -11,10 +12,12 @@
 import GcBreadcrumbs from "@/components/catalog/gcBreadcrumbs";
 import GcSort from "@/components/catalog/gcSort";
 import GcProducts from "@/components/catalog/gcProducts";
+import GcPagination from "@/components/catalog/gcPagination";
 
 export default {
   name: "Catalog",
   components: {
+    GcPagination,
     GcProducts,
     GcSort,
     GcBreadcrumbs
