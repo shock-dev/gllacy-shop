b225edbdbccd9a80341fc518ccb3840ed5f7a288
Andrey <shock.work@mail.ru>
2021-01-10 16:38:19 +0300

    template for cart popup

--- src/components/app/gcHeader.vue
+++ src/components/app/gcHeader.vue
@@ -25,6 +25,7 @@
             <button
               @click="openPopup('search')"
               class="header__button search"
+              :class="{ active: isSearchFormOpen }"
             >
               <svg width="17" height="17">
                 <use href="~@/assets/img/sprite.svg#search"></use>
@@ -38,6 +39,7 @@
             <button
               class="header__button login flex align"
               @click="openPopup('login')"
+              :class="{ active: isLoginFormOpen }"
             >
               <svg width="21" height="19">
                 <use href="~@/assets/img/sprite.svg#login"></use>
@@ -56,18 +58,62 @@
               </div>
             </div>
           </div>
-          <button v-if="!products.length" class="header__button cart flex align disabled">
-            <svg width="21" height="20">
-              <use href="~@/assets/img/sprite.svg#cart"></use>
-            </svg>
-            <span class="header__button-text">Пусто</span>
-          </button>
-          <button v-else class="header__button cart flex align">
-            <svg width="21" height="20">
-              <use href="~@/assets/img/sprite.svg#cart"></use>
-            </svg>
-            <span class="header__button-text">{{ products.length }} товара</span>
-          </button>
+          <div class="header__button-wrapper">
+            <button v-if="!products.length" class="header__button cart flex align disabled">
+              <svg width="21" height="20">
+                <use href="~@/assets/img/sprite.svg#cart"></use>
+              </svg>
+              <span class="header__button-text">Пусто</span>
+            </button>
+            <button
+              v-else
+              class="header__button cart flex align active"
+              @click="openPopup('cart')"
+            >
+              <svg width="21" height="20">
+                <use href="~@/assets/img/sprite.svg#cart"></use>
+              </svg>
+              <span class="header__button-text">{{ products.length }} товара</span>
+            </button>
+            <div class="cart-popup" :class="{ active: isCartOpen }">
+              <ul class="cart-popup__list">
+                <li class="cart-popup__item flex align">
+                  <button class="cart-popup__item-del">
+                    <img src="~@/assets/img/cross-small.svg" alt="Delete product">
+                  </button>
+                  <div class="cart-popup__item-img">
+                    <img src="~@/assets/img/ice-cream-1.jpg" alt="">
+                  </div>
+                  <div class="cart-popup__item-title">
+                    Пломбир с апельсиновым джемом
+                  </div>
+                  <div class="cart-popup__item-consider">
+                    1,5 кг х <span>200 руб.</span>
+                  </div>
+                  <div class="cart-popup__item-price">300 руб.</div>
+                </li>
+                <li class="cart-popup__item flex align">
+                  <button class="cart-popup__item-del">
+                    <img src="~@/assets/img/cross-small.svg" alt="Delete product">
+                  </button>
+                  <div class="cart-popup__item-img">
+                    <img src="~@/assets/img/ice-cream-1.jpg" alt="">
+                  </div>
+                  <div class="cart-popup__item-title">
+                    Пломбир с апельсиновым джемом
+                  </div>
+                  <div class="cart-popup__item-consider">
+                    1,5 кг х <span>200 руб.</span>
+                  </div>
+                  <div class="cart-popup__item-price">300 руб.</div>
+                </li>
+              </ul>
+              <div class="cart-popup__footer">
+                <div class="cart-popup__footer-total">Итого: 750 руб.</div>
+                <button class="cart-popup__button def-btn">Оформить заказ</button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -87,7 +133,8 @@ export default {
   name: "gcHeader",
   data: () => ({
     isSearchFormOpen: false,
-    isLoginFormOpen: false
+    isLoginFormOpen: false,
+    isCartOpen: false
   }),
   computed: {
     ...mapGetters('cart', {
@@ -96,19 +143,24 @@ export default {
   },
   methods: {
     openPopup(elem) {
-      if (elem === 'search') {
-        this.isLoginFormOpen = false
-        this.isSearchFormOpen = !this.isSearchFormOpen
-      } else if (elem === 'login') {
-        this.isSearchFormOpen = false
-        this.isLoginFormOpen = !this.isLoginFormOpen
+      switch (elem) {
+        case 'search':
+          this.isCartOpen = false
+          this.isLoginFormOpen = false
+          this.isSearchFormOpen = !this.isSearchFormOpen
+          break
+        case 'login':
+          this.isCartOpen = false
+          this.isSearchFormOpen = false
+          this.isLoginFormOpen = !this.isLoginFormOpen
+          break
+        case 'cart':
+          this.isSearchFormOpen = false
+          this.isLoginFormOpen = false
+          this.isCartOpen = !this.isCartOpen
+          break
       }
     }
-  },
-  created() {
-    const onClickOutside = e => this.isSearchFormOpen = this.$el.contains(e.target) && this.isSearchFormOpen;
-    document.addEventListener('click', onClickOutside);
-    this.$on('hook:beforeDestroy', () => document.removeEventListener('click', onClickOutside));
-  },
+  }
 }
 </script>
