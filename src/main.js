b6d6f791229ef9a0032f5adb890b02fe7777c9f0
Andrey <shock.work@mail.ru>
2021-01-11 10:24:04 +0300

    install vue-meta and init it

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueMeta from 'vue-meta'
+
+Vue.use(VueMeta)
 
 Vue.config.productionTip = false
 
