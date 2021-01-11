e0d16da2acad0a2d0cbde585296ae44edbd16feb
Andrey <shock.work@mail.ru>
2021-01-11 08:10:26 +0300

    integrate it into header

--- src/components/app/header/gcHeader.vue
+++ src/components/app/header/gcHeader.vue
@@ -23,61 +23,7 @@
         <div class="header__buttons flex align">
           <gc-search-form/>
           <gc-login-form/>
-          <div class="header__button-wrapper">
-            <button v-if="!products.length" class="header__button cart flex align disabled">
-              <svg width="21" height="20">
-                <use href="~@/assets/img/sprite.svg#cart"></use>
-              </svg>
-              <span class="header__button-text">Пусто</span>
-            </button>
-            <button
-              v-else
-              class="header__button cart flex align active"
-            >
-              <svg width="21" height="20">
-                <use href="~@/assets/img/sprite.svg#cart"></use>
-              </svg>
-              <span class="header__button-text">{{ products.length }} товара</span>
-            </button>
-            <div class="cart-popup" :class="{ active: isCartOpen }">
-              <ul class="cart-popup__list">
-                <li class="cart-popup__item flex align">
-                  <button class="cart-popup__item-del">
-                    <img src="~@/assets/img/cross-small.svg" alt="Delete product">
-                  </button>
-                  <div class="cart-popup__item-img">
-                    <img src="~@/assets/img/ice-cream-1.jpg" alt="">
-                  </div>
-                  <div class="cart-popup__item-title">
-                    Пломбир с апельсиновым джемом
-                  </div>
-                  <div class="cart-popup__item-consider">
-                    1,5 кг х <span>200 руб.</span>
-                  </div>
-                  <div class="cart-popup__item-price">300 руб.</div>
-                </li>
-                <li class="cart-popup__item flex align">
-                  <button class="cart-popup__item-del">
-                    <img src="~@/assets/img/cross-small.svg" alt="Delete product">
-                  </button>
-                  <div class="cart-popup__item-img">
-                    <img src="~@/assets/img/ice-cream-1.jpg" alt="">
-                  </div>
-                  <div class="cart-popup__item-title">
-                    Пломбир с апельсиновым джемом
-                  </div>
-                  <div class="cart-popup__item-consider">
-                    1,5 кг х <span>200 руб.</span>
-                  </div>
-                  <div class="cart-popup__item-price">300 руб.</div>
-                </li>
-              </ul>
-              <div class="cart-popup__footer">
-                <div class="cart-popup__footer-total">Итого: 750 руб.</div>
-                <button class="cart-popup__button def-btn">Оформить заказ</button>
-              </div>
-            </div>
-          </div>
+          <gc-cart/>
         </div>
       </div>
     </div>
@@ -91,20 +37,16 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
 import GcSearchForm from "@/components/app/header/gcSearchForm";
 import GcLoginForm from "@/components/app/header/gcLoginForm";
+import GcCart from "@/components/app/header/gcCart";
 
 export default {
   name: "gcHeader",
   components: {
+    GcCart,
     GcLoginForm,
     GcSearchForm
-  },
-  computed: {
-    ...mapGetters('cart', {
-      products: 'items'
-    })
   }
 }
 </script>
