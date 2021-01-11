784b6a40c77fe907adc08f49e29d2c001e9dd6ac
Andrey <shock.work@mail.ru>
2021-01-11 07:57:37 +0300

    integrate it into header

--- src/components/app/header/gcHeader.vue
+++ src/components/app/header/gcHeader.vue
@@ -22,29 +22,7 @@
       <div class="header__right">
         <div class="header__buttons flex align">
           <gc-search-form/>
-          <div class="header__button-wrapper">
-            <button
-              class="header__button login flex align"
-              @click="openPopup('login')"
-              :class="{ active: isLoginFormOpen }"
-            >
-              <svg width="21" height="19">
-                <use href="~@/assets/img/sprite.svg#login"></use>
-              </svg>
-              <span class="header__button-text">Вход</span>
-            </button>
-            <div class="login-form" :class="{ active: isLoginFormOpen }">
-              <input class="search-form__field" type="text" aria-label="email" placeholder="email@example.com">
-              <input class="search-form__field" type="password" aria-label="password" placeholder="••••••••">
-              <div class="login-form__footer flex">
-                <button class="login-form__button def-btn">Войти</button>
-                <div class="login-form__actions align">
-                  <router-link class="login-form__action" to="#">Забыли пароль?</router-link>
-                  <router-link class="login-form__action" to="#">Новая регистрация</router-link>
-                </div>
-              </div>
-            </div>
-          </div>
+          <gc-login-form/>
           <div class="header__button-wrapper">
             <button v-if="!products.length" class="header__button cart flex align disabled">
               <svg width="21" height="20">
@@ -55,7 +33,6 @@
             <button
               v-else
               class="header__button cart flex align active"
-              @click="openPopup('cart')"
             >
               <svg width="21" height="20">
                 <use href="~@/assets/img/sprite.svg#cart"></use>
@@ -116,10 +93,12 @@
 <script>
 import { mapGetters } from 'vuex'
 import GcSearchForm from "@/components/app/header/gcSearchForm";
+import GcLoginForm from "@/components/app/header/gcLoginForm";
 
 export default {
   name: "gcHeader",
   components: {
+    GcLoginForm,
     GcSearchForm
   },
   computed: {
