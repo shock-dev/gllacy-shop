5c272d3100aacd4c7311531767f5088eb61e1264
Andrey <shock.work@mail.ru>
2021-01-13 12:58:33 +0300

    *sort head template

--- src/views/Catalog.vue
+++ src/views/Catalog.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="catalog">
     <gc-breadcrumbs/>
-    <h2 class="catalog__title">Сливочное мороженое</h2>
+    <div class="catalog__head flex align between">
+      <h2 class="catalog__title">Сливочное мороженое</h2>
+      <button class="catalog__btn-filter" @click="openSort">
+        <svg>
+            <use href="~@/assets/img/sprite.svg#filter"></use>
+        </svg>
+      </button>
+    </div>
     <gc-sort/>
     <gc-products/>
     <gc-pagination/>
@@ -13,11 +20,20 @@ import GcBreadcrumbs from "@/components/catalog/gcBreadcrumbs";
 import GcSort from "@/components/catalog/gcSort";
 import GcProducts from "@/components/catalog/gcProducts";
 import GcPagination from "@/components/catalog/gcPagination";
+import { mapMutations } from 'vuex'
 
 export default {
   name: "Catalog",
   metaInfo: {
     title: 'Gllacy shop - catalog'
+  },
+  methods: {
+    ...mapMutations(['toggleOverlay', 'toggleSort']),
+    openSort() {
+      document.body.classList.add('lock')
+      this.toggleOverlay()
+      this.toggleSort()
+    }
   },
   components: {
     GcPagination,
