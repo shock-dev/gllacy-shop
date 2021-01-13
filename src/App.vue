1e81ca318cfa2f33ca4c951231dcfb04adcf8647
Andrey <shock.work@mail.ru>
2021-01-13 12:57:49 +0300

    import overlay in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app" class="container">
+    <overlay/>
     <gc-header/>
     <router-view/>
     <gc-footer/>
@@ -9,9 +10,11 @@
 <script>
 import GcHeader from "@/components/app/header/gcHeader";
 import GcFooter from "@/components/app/gcFooter";
+import Overlay from "@/components/app/overlay";
 
 export default {
   components: {
+    Overlay,
     GcFooter,
     GcHeader
   }
